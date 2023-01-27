63e579f7c9da63a8cac0219c4d6d66f5cd7fa4ca
grishagr9 <56480294+grishagr9@users.noreply.github.com>
2023-01-28 02:55:49 +0300

    Add files via upload

---
+++ AffineTransformations/Polygon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace AffineTransformations
+{
+    public class Polygon
+    {
+        public List<Edge> Edges { get; private set; }
+
+        public Polygon(List<Edge> edges)
+        {
+            Edges = edges;
+        }
+
+        public void ToRed() {
+            foreach (var item in Edges)
+                item.Red = true;
+        }
+        public void ToUsual()
+        {
+            foreach (var item in Edges)
+                item.Red = false;
+        }
+        // TODO: реализовать методы для полигона. При необходимости реализовать методы для ребра и вызывать в полигоне
+        // (например, для поворота полигона нужно реализовать поворот ребра).
+    }
+}
